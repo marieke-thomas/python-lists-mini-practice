2495670502312af280f01ee8c105e435c111d752
EC2 Default User <ec2-user@ip-172-31-6-126.us-east-2.compute.internal>
2019-07-17 21:53:11 +0000

    Updated lab

---
+++ politicians.py
@@ -0,0 +1,20 @@
+politicians = [
+    {"firstName":"Bernie","lastName":"Sanders","country":"USA","politicalViews":"Democratic Socialist","quote":"We must create an economy that works for all, not just the very wealthy"},
+    {"firstName":"Karl","lastName":"Marx","country":"Germany","politicalViews":"Communist","quote":"The proletariat have nothing to lose but their chains"},
+    {"firstName":"Barack","lastName":"Obama","country":"USA","politicalViews":"Democrat","quote":"What I'm asking for is hard. It's easier to be cynical; to accept that change isn't possible, and politics is hopeless, and to believe that our voices and actions don't matter. But if we give up now, then we forsake a better future."},
+    ]
+
+
+# CHALLENGES:
+# 1. Use list/dictionary methods to write code that will print Bernie's country
+
+
+
+# 2. Use list/dictionary methods to write code that will print Marx's quote
+
+
+# 3. Change Obama's quote to the new quote below:
+new_quote = "My fellow Americans, we are and always will be a nation of immigrants. We were strangers once, too."
+
+
+# 4. Add a new politician to the list
